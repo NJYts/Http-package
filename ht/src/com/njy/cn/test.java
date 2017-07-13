89d3d64ff6bdc15c235b862e65131572a05acaed
tsnjy <tsnjy@126.com>
2017-07-13 13:44:02 +0800

    hello world!

--- ht/src/com/njy/cn/test.java
+++ ht/src/com/njy/cn/test.java
@@ -4,4 +4,12 @@ package com.njy.cn;
  * Created by WISE on 2017/7/12.
  */
 public class test {
+    @Override
+    public String toString() {
+        return super.toString();
+    }
+
+    public static void main(String[] args) {
+        System.out.println("hello world !");
+    }
 }
